5e1246842c5432782ba015efe0e10febeffa9ff2
Carlo dela Fuente <gdelafuente1@my.bcit.ca>
2025-04-30 16:31:10 -0700

    created functions, CPT, js folders

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function portfolio_enqueues() {
+
+	wp_enqueue_style( 
+		'portfolio-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' ),
+		'all'
+	);
+
+    wp_enqueue_style( 
+        'school-normalize', 
+        'https://unpkg.com/@csstools/normalize.css', 
+        array(), 
+        '12.1.0'
+    );
+
+
+
+}
+add_action('wp_enqueue_scripts', 'portfolio_enqueues');
+
+require get_template_directory() . '/inc/post-types-taxonomies.php';
